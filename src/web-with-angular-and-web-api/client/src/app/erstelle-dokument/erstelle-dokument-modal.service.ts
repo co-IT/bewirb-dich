a8a4a370a25e90c45c563594dbc9f5f981885c58
Frank Strieter <fs@co-it.eu>
2023-01-18 16:13:03 +0100

    fix: dokumententyp

--- src/web-with-angular-and-web-api/client/src/app/erstelle-dokument/erstelle-dokument-modal.service.ts
+++ src/web-with-angular-and-web-api/client/src/app/erstelle-dokument/erstelle-dokument-modal.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Dokument } from '../models/dokument';
+import { Dokument, Dokumententyp } from '../models/dokument';
 
 @Injectable({ providedIn: 'root' })
 export class ErstelleDokumentModalService {
@@ -27,7 +27,7 @@ export class ErstelleDokumentModalService {
 
   async close(save: boolean) {
       if(save) {
-          await this.httpClient.post(environment.baseurl + '/dokumente/erstellen', this.dokument).toPromise()
+          await this.httpClient.post(environment.baseurl + '/dokumente/erstellen', {...this.dokument, typ: Dokumententyp.ANGEBOT}).toPromise()
           this.saved.emit();
       }
       this.modal.close();
